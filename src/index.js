b11f6dddb332730bbcb277ed7a918f293b5af16e
kaddlz <roslanov@icloud.com>
2023-10-30 01:41:58 +0300

    feat: complete morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let chunks = expr.match(/.{1,10}/g);
+    let message = "";
+    for (let chunk of chunks) {
+        if (chunk === "**********") {
+            message += " ";
+        } else {
+            let trimmed = chunk.replace(/^0+/, "");
+            let morse = trimmed.replace(/10/g, ".").replace(/11/g, "-");
+            let letter = MORSE_TABLE[morse];
+            message += letter;
+        }
+    }
+    return message;
 }
+
+module.exports = {
+    decode
+}
+
+
 
 module.exports = {
     decode
